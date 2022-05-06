53dc0cd67ecca42dd1d44fe878096ec218c99773
kulinsky <juliankulinsky@web.de>
2022-05-06 13:23:32 +0200

    feat: setting up scraping environment

---
+++ src/scraper.py
@@ -0,0 +1,16 @@
+import requests
+
+
+# Crawling web page with given URL
+def get(url):
+    res = requests.get(url)
+    if res.status_code == 200:
+        return res.text.strip()
+    else:
+        return ''
+
+# Parsing html
+def get_news_headline():
+
+
+
