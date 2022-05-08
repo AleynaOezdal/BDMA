8d8b7972237158bf39c0dcbc8315d2d9deb1b33b
kulinsky <juliankulinsky@web.de>
2022-05-08 20:55:51 +0200

    feat: print headlines on 3 DAX40 companies as strings on console

--- src/scraper.py
+++ src/scraper.py
@@ -10,22 +10,24 @@ def get(url):
         return 'Error in URL Status Code'
 
 # Parsing html
-def get_news_headlines_from_finanzennet(url):
-    soup = bs(get(url), 'html.parser')
+def get_news_headlines_from_finanzennet():
+    dax40_companies = ['adidas', 'sap', 'bmw']
 
-    # Top Headlines on website
-    content = soup.find('span', attrs={"class": "teaser-headline"})
-    print(content.text)
+    for company in dax40_companies:
+        base_url = f'https://www.finanzen.net/news/{company}-news'
+        print(f"Now following company: {company}")
+        soup = bs(get(base_url), 'html.parser')
 
-    # List Headlines on website
-    found_news = soup.find_all("a", attrs={"class": "teaser"})
-    for headline in found_news:
-        print(f"{headline.text}")
+        # Top Headlines on website
+        content = soup.find('span', attrs={"class": "teaser-headline"})
+        print(content.text)
+
+        # List Headlines on website
+        found_news = soup.find_all("a", attrs={"class": "teaser"})
+        for headline in found_news:
+            print(f"{headline.text}")
+        print(f"+++ Finished Company: {company} +++\n")
 
 
 if __name__ == '__main__':
-    dax40_companies = ['adidas', 'sap', 'bmw']
-    for company in dax40_companies:
-        print(f"Now following company: {company}\n")
-        get_news_headlines_from_finanzennet(f'https://www.finanzen.net/news/{company}-news')
-        print('-------- END ---------.\n')
+    get_news_headlines_from_finanzennet()
