39dbe228f11774f00fbec6c2911bcdd85298e948
kulinsky <juliankulinsky@web.de>
2022-05-18 18:13:36 +0200

    feat: spark integration - WIP

--- src/scraper.py
+++ src/scraper.py
@@ -3,13 +3,16 @@ from bs4 import BeautifulSoup as bs
 import yfinance as yf
 import pprint as pp
 
+
 # Source for DAX Symbols: https://de.wikipedia.org/wiki/DAX#Zusammensetzung
-yfinance_symbols_dax_companies = ['ads', 'air', 'alv', 'bas', 'bayn', 'bmw', 'bnr',
-             'con', '1cov', 'dtg', 'dher', 'dbk', 'db1', 'dpw',
-             'dte', 'eoan', 'fre', 'fme', 'hnr1', 'hei', 'hfg',
-             'hen3', 'ifx', 'lin', 'mbg', 'mrk', 'mtx', 'muv2',
-             'pah3', 'pum', 'qia', 'rwe', 'sap', 'srt3', 'sie',
-             'shl', 'sy1', 'vow3', 'vna', 'zal']
+yfinance_symbols_dax_companies = [
+     'ads', 'air', 'alv', 'bas', 'bayn', 'bmw', 'bnr',
+     'con', '1cov', 'dtg', 'dher', 'dbk', 'db1', 'dpw',
+     'dte', 'eoan', 'fre', 'fme', 'hnr1', 'hei', 'hfg',
+     'hen3', 'ifx', 'lin', 'mbg', 'mrk', 'mtx', 'muv2',
+     'pah3', 'pum', 'qia', 'rwe', 'sap', 'srt3', 'sie',
+     'shl', 'sy1', 'vow3', 'vna', 'zal'
+]
 
 test_symbols = ['ads', 'air', 'alv']
 
