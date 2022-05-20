c36d3e58fd3ede1f5f25a7a6b89bebe765de439a
kulinsky <juliankulinsky@web.de>
2022-05-20 10:44:19 +0200

    feat: integrated static scraper - possible now to extract WKN/ ISIN for each company (static)

--- src/static_scraper.py
+++ src/static_scraper.py
@@ -5,6 +5,8 @@ Here we will extract data from the internet which won't change in June 2022
 
 from bs4 import BeautifulSoup as bs
 from scraper import yfinance_symbols_dax_companies, initialize_yf_tickers, get
+# from producersetup import p, topic, delivery_report
+# import json
 
 
 all_companies = [
@@ -29,11 +31,13 @@ def get_WKN_and_ISIN(companies: list = ['adidas', 'volkswagen']):
             identification_number = soup.find('span', attrs={'class': 'instrument-id'})
             #Store company as key and WKN/ISIN as value
             all_wkns_and_isins[f'{company}'] = identification_number.text
+            #p.produce(topic, json.dumps(dict({company: all_wkns_and_isins[f'{company}']})), callback=delivery_report)
+            #p.flush()
         except Exception as e:
             all_wkns_and_isins[f'{company}'] = "NaN"
-            print(f"For {company} the following error occured: {type(e)}")
+            print(f"FAILED. For {company} the following error occured: {type(e)}")
 
-    return all_wkns_and_isins
+    return True
 
 if __name__ == '__main__':
     print(get_WKN_and_ISIN(all_companies))
