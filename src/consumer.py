60a888a2a48bb57c368a5be664b9d98aaee2ddb6
kulinsky <juliankulinsky@web.de>
2022-05-24 22:11:21 +0200

    feat: basic consumer

--- src/consumer.py
+++ src/consumer.py
@@ -1,38 +1,48 @@
 from confluent_kafka import Consumer
 import os
 from dotenv import load_dotenv
-from producersetup import topic
-import findspark
-from pyspark.sql import SparkSession
-
+from static_scraper import financial_KPIs, get_kpi_topic
 
 load_dotenv()
-#findspark.init()
-#spark = SparkSession.builder.master("local[*]").appName("pySparkBDMA").getOrCreate()
 
-c = Consumer({
-    'bootstrap.servers': os.getenv("BOOTSTRAP.SERVERS"),
-    'security.protocol': os.getenv("SECURITY.PROTOCOL"),
-    'sasl.mechanisms': os.getenv("SASL.MECHANISMS"),
-    'sasl.username': os.getenv("SASL.USERNAME"),
-    'sasl.password': os.getenv("SASL.PASSWORD"),
-    'group.id': 'mygroup',
-    'auto.offset.reset': 'earliest'
-})
 
-c.subscribe([f'{topic}'])
+def create_consumers_for_topics(topics: list):
+    all_messages_for_topic = list()
+    for topic in topics:
 
-while True:
-    msg = c.poll(1.0)
+        c = Consumer(
+            {
+                "bootstrap.servers": os.getenv("BOOTSTRAP.SERVERS"),
+                "security.protocol": os.getenv("SECURITY.PROTOCOL"),
+                "sasl.mechanisms": os.getenv("SASL.MECHANISMS"),
+                "sasl.username": os.getenv("SASL.USERNAME"),
+                "sasl.password": os.getenv("SASL.PASSWORD"),
+                "group.id": topic,
+                "auto.offset.reset": "earliest",
+            }
+        )
 
-    if msg is None:
-        continue
-    if msg.error():
-        print(f"Consumer error: {msg.error}")
-        continue
+        c.subscribe([f'{get_kpi_topic(topic)}'])
 
-    print(f"Received message: {msg.value().decode('utf-8')}")
-    #simple_rdd = spark.sparkContext.parallelize(msg.value().decode('utf-8'))
-    #print(simple_rdd.collect())
+        i = 0
+        while i < 39:
+            msg = c.poll(1.0)
 
-c.close()
+            if msg is None:
+                continue
+            if msg.error():
+                print(f"Consumer error: {msg.error}")
+                continue
+
+            # Do something with message
+            all_messages_for_topic.append(msg.value().decode('utf-8'))
+            i += 1
+
+        c.close()
+
+    return all_messages_for_topic
+
+
+if __name__ == "__main__":
+    # create_consumers_for_topics(financial_KPIs[:-1])
+    pass
---
+++ src/insert_to_mongo.py
@@ -0,0 +1,6 @@
+from consumer import create_consumers_for_topics
+import pprint
+
+
+if __name__ == '__main__':
+    pprint.pprint(create_consumers_for_topics(['Total Operating Expenses']))
