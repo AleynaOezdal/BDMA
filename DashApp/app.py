2a6d5e4e9ac1fc2e34a46c147ac0c553891eac88
kimrbg <kim-rautenberg@gmx.de>
2022-05-30 21:30:00 +0200

    updated header

--- DashApp/app.py
+++ DashApp/app.py
@@ -16,10 +16,23 @@ font = {
     'helvetica' : 'Arial, Helvetica, sans-serif'
 }
 
+from datetime import datetime
+# datetime object containing current date and time
+now = datetime.now()
+
+dt_string = now.strftime("%H")
+
+if int(dt_string) >= 0 and int(dt_string) <= 9:
+    greeting = 'Guten Morgen '
+elif int(dt_string) >= 10 and int(dt_string) <= 17:
+    greeting = 'Guten Tag '
+elif int(dt_string) >= 18 and int(dt_string) <= 23:
+    greeting = 'Guten Abend '
+
 #header
 header = html.Div(id = 'Header', children= [
     html.H1(children='DAX40 - das Unternehmer-Dashboard'),
-    html.Div(children='Guten Tag Herr. CEO.'),
+    html.Div(children=[greeting+'Herr. CEO.']),
     html.Div(children='Team DAX40 wünscht Ihnen einen erfolgreichen Tag mit richtigen Entscheidungen!')])
 
 #content
