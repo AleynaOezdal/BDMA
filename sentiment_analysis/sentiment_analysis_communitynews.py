686cdc28da0d1df82e771b45953236bb2cd8e54d
Hannah Hielscher <hh057@hdm-stuttgart.de>
2022-05-31 17:21:24 +0200

    update pep8

--- sentiment_analysis/sentiment_analysis_communitynews.py
+++ sentiment_analysis/sentiment_analysis_communitynews.py
@@ -17,54 +17,62 @@ connectionString = f"mongodb+srv://{os.getenv('MONGODB.USERNAME')}:{os.getenv('M
 
 # Konfiguration von pyspark
 conf = pyspark.SparkConf()
-conf.set("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector:10.0.2,com.johnsnowlabs.nlp:spark-nlp_2.12:3.4.4")
+conf.set(
+    "spark.jars.packages",
+    "org.mongodb.spark:mongo-spark-connector:10.0.2,com.johnsnowlabs.nlp:spark-nlp_2.12:3.4.4",
+)
 
 # SparkSession mit allen benötigten Konfigurationen (MongoDB und SparkNLP) erstellen
-spark = SparkSession.builder \
-    .appName("Spark NLP")\
-    .master("local[4]")\
-    .config("spark.driver.memory","16G") \
-    .config("spark.driver.maxResultSize", "0") \
-    .config("spark.kryoserializer.buffer.max", "2000M")\
-    .config('spark.mongodb.read.connection.uri', connectionString)\
-    .config('spark.mongodb.write.connection.uri', connectionString)\
-    .config(conf=conf) \
+spark = (
+    SparkSession.builder.appName("Spark NLP")
+    .master("local[4]")
+    .config("spark.driver.memory", "16G")
+    .config("spark.driver.maxResultSize", "0")
+    .config("spark.kryoserializer.buffer.max", "2000M")
+    .config("spark.mongodb.read.connection.uri", connectionString)
+    .config("spark.mongodb.write.connection.uri", connectionString)
+    .config(conf=conf)
     .getOrCreate()
+)
 
 # Auslesen der Collection community_news in Spark DataFrame
-df = spark.read.format("mongodb").option("database", "Company-Experience").option("collection", "Community_news").load()
+df = (
+    spark.read.format("mongodb")
+    .option("database", "Company-Experience")
+    .option("collection", "Community_news")
+    .load()
+)
 
 # DataFrame in gewünschte Struktur bringen, Verschachtelungen glätten
-df2 = df.select(col("_id"),
+df2 = df.select(
+    col("_id"),
     col("community_news.more info"),
     col("community_news.User"),
     col("community_news.message"),
     col("community_news.timestamp"),
-    col("time"))
-df2Flatten = df2.toDF("id","more_info","user","message","time","timestamp")
+    col("time"),
+)
+df2Flatten = df2.toDF("id", "more_info", "user", "message", "time", "timestamp")
 
 # SparkNLP Model Pipeline:
-document = DocumentAssembler() \
-    .setInputCol("message") \
-    .setOutputCol("document")
+document = DocumentAssembler().setInputCol("message").setOutputCol("document")
 
-embeddings = BertSentenceEmbeddings\
-    .pretrained('labse', 'xx') \
-    .setInputCols(["document"]) \
+embeddings = (
+    BertSentenceEmbeddings.pretrained("labse", "xx")
+    .setInputCols(["document"])
     .setOutputCol("sentence_embeddings")
+)
 
-sentimentClassifier = ClassifierDLModel.pretrained("classifierdl_bert_sentiment", "de") \
-    .setInputCols(["document", "sentence_embeddings"]) \
+sentimentClassifier = (
+    ClassifierDLModel.pretrained("classifierdl_bert_sentiment", "de")
+    .setInputCols(["document", "sentence_embeddings"])
     .setOutputCol("class")
+)
 
-nlpPipeline = Pipeline(stages=[
- document,
- embeddings,
- sentimentClassifier
- ])
+nlpPipeline = Pipeline(stages=[document, embeddings, sentimentClassifier])
 
 # Leeren DataFrame für Ergebnisse erstellen
-empty_df = spark.createDataFrame([['']]).toDF("text")
+empty_df = spark.createDataFrame([[""]]).toDF("text")
 
 # Pipeline auf leeren DataFrame fitten
 pipelineModel = nlpPipeline.fit(empty_df)
@@ -72,20 +80,26 @@ pipelineModel = nlpPipeline.fit(empty_df)
 # Pipeline auf DataFrame mit Daten anwenden
 result = pipelineModel.transform(df2Flatten)
 
-# DataFrame für Ergebnisse erstellen, welches an Datenbank gesendet werden kann
-df_results = result.select(col("id"),
+# DataFrame für Ergebnisse erstellen, welches an Datenbank gesendet werden kann: Enthält alle Infos wie EingangsDF plus Klasse
+df_results = result.select(
+    col("id"),
     col("more_info"),
     col("user"),
     col("message"),
     col("time"),
     col("timestamp"),
-    col("class.result"))
+    col("class.result"),
+)
 
-df_resultsFlatten = df_results.toDF("id", "more_info", "user", "message", "time", "timestamp", "class")
+df_resultsFlatten = df_results.toDF(
+    "id", "more_info", "user", "message", "time", "timestamp", "class"
+)
 
 # Entfernen der eckigen Klammern um Klasse
-finished_df = df_resultsFlatten.withColumn('class', F.explode('class'))
-#finished_df.show()
+finished_df = df_resultsFlatten.withColumn("class", F.explode("class"))
+# finished_df.show()
 
 # DataFrame in Datenbank schreiben, collection Community_news_sentiment
-finished_df.write.format("mongodb").mode("append").option("database", "Company-Experience").option("collection", "Community_news_sentiment").save()
+finished_df.write.format("mongodb").mode("append").option(
+    "database", "Company-Experience"
+).option("collection", "Community_news_sentiment").save()
--- sentiment_analysis/sentiment_analysis_customerexperience.py
+++ sentiment_analysis/sentiment_analysis_customerexperience.py
@@ -17,54 +17,62 @@ connectionString = f"mongodb+srv://{os.getenv('MONGODB.USERNAME')}:{os.getenv('M
 
 # Konfiguration von pyspark
 conf = pyspark.SparkConf()
-conf.set("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector:10.0.2,com.johnsnowlabs.nlp:spark-nlp_2.12:3.4.4")
+conf.set(
+    "spark.jars.packages",
+    "org.mongodb.spark:mongo-spark-connector:10.0.2,com.johnsnowlabs.nlp:spark-nlp_2.12:3.4.4",
+)
 
 # SparkSession mit allen benötigten Konfigurationen (MongoDB und SparkNLP) erstellen
-spark = SparkSession.builder \
-    .appName("Spark NLP")\
-    .master("local[4]")\
-    .config("spark.driver.memory","16G") \
-    .config("spark.driver.maxResultSize", "0") \
-    .config("spark.kryoserializer.buffer.max", "2000M")\
-    .config('spark.mongodb.read.connection.uri', connectionString)\
-    .config('spark.mongodb.write.connection.uri', connectionString)\
-    .config(conf=conf) \
+spark = (
+    SparkSession.builder.appName("Spark NLP")
+    .master("local[4]")
+    .config("spark.driver.memory", "16G")
+    .config("spark.driver.maxResultSize", "0")
+    .config("spark.kryoserializer.buffer.max", "2000M")
+    .config("spark.mongodb.read.connection.uri", connectionString)
+    .config("spark.mongodb.write.connection.uri", connectionString)
+    .config(conf=conf)
     .getOrCreate()
+)
 
 # Auslesen der Collection Customer_experience in Spark DataFrame
-df = spark.read.format("mongodb").option("database", "Company-Experience").option("collection", "Customer_experience").load()
+df = (
+    spark.read.format("mongodb")
+    .option("database", "Company-Experience")
+    .option("collection", "Customer_experience")
+    .load()
+)
 
 # DataFrame in gewünschte Struktur bringen, Verschachtelungen glätten
-df2 = df.select(col("_id"),
+df2 = df.select(
+    col("_id"),
     col("customer_exp.more info"),
     col("customer_exp.review"),
     col("customer_exp.title"),
     col("customer_exp.time"),
-    col("time"))
-df2Flatten = df2.toDF("id","more_info","review","title","time","timestamp")
+    col("time"),
+)
+df2Flatten = df2.toDF("id", "more_info", "review", "title", "time", "timestamp")
 
 # SparkNLP Model Pipeline:
-document = DocumentAssembler() \
-    .setInputCol("review") \
-    .setOutputCol("document")
+document = DocumentAssembler().setInputCol("review").setOutputCol("document")
 
-embeddings = BertSentenceEmbeddings\
-    .pretrained('labse', 'xx') \
-    .setInputCols(["document"]) \
+embeddings = (
+    BertSentenceEmbeddings.pretrained("labse", "xx")
+    .setInputCols(["document"])
     .setOutputCol("sentence_embeddings")
+)
 
-sentimentClassifier = ClassifierDLModel.pretrained("classifierdl_bert_sentiment", "de") \
-    .setInputCols(["document", "sentence_embeddings"]) \
+sentimentClassifier = (
+    ClassifierDLModel.pretrained("classifierdl_bert_sentiment", "de")
+    .setInputCols(["document", "sentence_embeddings"])
     .setOutputCol("class")
+)
 
-nlpPipeline = Pipeline(stages=[
- document,
- embeddings,
- sentimentClassifier
- ])
+nlpPipeline = Pipeline(stages=[document, embeddings, sentimentClassifier])
 
 # Leeren DataFrame für Ergebnisse erstellen
-empty_df = spark.createDataFrame([['']]).toDF("text")
+empty_df = spark.createDataFrame([[""]]).toDF("text")
 
 # Pipeline auf leeren DataFrame fitten
 pipelineModel = nlpPipeline.fit(empty_df)
@@ -72,20 +80,26 @@ pipelineModel = nlpPipeline.fit(empty_df)
 # Pipeline auf DataFrame mit Daten anwenden
 result = pipelineModel.transform(df2Flatten)
 
-# DataFrame für Ergebnisse erstellen, welches an Datenbank gesendet werden kann
-df_results = result.select(col("id"),
+# DataFrame für Ergebnisse erstellen, welches an Datenbank gesendet werden kann: Enthält alle Infos wie EingangsDF plus Klasse
+df_results = result.select(
+    col("id"),
     col("more_info"),
     col("review"),
     col("title"),
     col("time"),
     col("timestamp"),
-    col("class.result"))
+    col("class.result"),
+)
 
-df_resultsFlatten = df_results.toDF("id", "more_info", "review", "title", "time", "timestamp", "class")
+df_resultsFlatten = df_results.toDF(
+    "id", "more_info", "review", "title", "time", "timestamp", "class"
+)
 
 # Entfernen der eckigen Klammern um Klasse
-finished_df = df_resultsFlatten.withColumn('class', F.explode('class'))
-#finished_df.show()
+finished_df = df_resultsFlatten.withColumn("class", F.explode("class"))
+# finished_df.show()
 
 # DataFrame in Datenbank schreiben, collection Customer_experience_sentiment
-finished_df.write.format("mongodb").mode("append").option("database", "Company-Experience").option("collection", "Customer_experience_sentiment").save()
+finished_df.write.format("mongodb").mode("append").option(
+    "database", "Company-Experience"
+).option("collection", "Customer_experience_sentiment").save()
