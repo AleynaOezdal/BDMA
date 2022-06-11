63cc105eef38854bdfb84874058f6c663544a7ae
Hannah Hielscher <hh057@hdm-stuttgart.de>
2022-06-11 12:58:57 +0200

    consumers for kpis

---
+++ src/consumers/KPIs/consumerDax40dist.py
@@ -0,0 +1,33 @@
+import json
+
+from confluent_kafka import Consumer
+from consumers.KPIs.databasesetup_KPIs import get_database
+
+dbname = get_database()
+collection_name = dbname["dax40_distribution"]
+
+c = Consumer({
+    'bootstrap.servers' :'pkc-4ygn6.europe-west3.gcp.confluent.cloud:9092',
+    'security.protocol' : 'SASL_SSL',
+    'sasl.mechanisms' : 'PLAIN',
+    'sasl.username' : '2CV6Y6RATV64WJHF',
+    'sasl.password' : '6kLfk90tFiAMSX5awmnQUx8TptlbxNb25zUEObq9uJwY+6bZuIUl0KI71wNfwu7M',
+    'group.id': 'mygroup0402',
+    'auto.offset.reset': 'earliest'
+})
+
+c.subscribe(['dax40_distribution'])
+
+while True:
+    msg = c.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        print("Consumer error: {}".format(msg.error()))
+        continue
+
+    print('Received message: {}'.format(msg.value().decode('utf-8')))
+
+    collection_name.insert_one(json.loads(msg.value().decode('utf-8')))
+c.close()
---
+++ src/consumers/KPIs/databasesetup_KPIs.py
@@ -0,0 +1,25 @@
+def get_database():
+    from pymongo import MongoClient
+    import pymongo
+    import os
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    CONNECTION_STRING = f"mongodb+srv://{os.getenv('MONGODB.USERNAME')}:{os.getenv('MONGODB.PASSWORD')}@cluster0.hj2sr.mongodb.net"
+
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    from pymongo import MongoClient
+    client = MongoClient(CONNECTION_STRING)
+
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client["KPIs"]
+
+
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":
+    # Get the database
+    dbname = get_database()
+
+
