43e8bba4fb0455d4278463ebbf5052b2c995566a
kimrbg <kim-rautenberg@gmx.de>
2022-06-25 09:47:21 +0200

    updated news

--- DashApp/news.py
+++ DashApp/news.py
@@ -180,37 +180,37 @@ def get_table_rows_three(df):
     table_rows0 = html.Tr(id='table_tr', children=[
                     html.Td(id='table_td', children=[
                         html.A(id='table_td_link',children=df.iloc[0][0],href=df.iloc[0][2])]),
-                    html.Td(id='table_td', children=[get_thumbs(df.iloc[0][1])]),
+                    html.Td(id='table_td', children=[df.iloc[0][1]]),
                 ])
 
     table_rows1 = html.Tr(id='table_tr', children=[
                     html.Td(id='table_td', children=[
                         html.A(id='table_td_link',children=df.iloc[1][0],href=df.iloc[1][2])]),
-                    html.Td(id='table_td', children=[get_thumbs(df.iloc[1][1])]),
+                    html.Td(id='table_td', children=df.iloc[1][1]),
                 ])
     
     table_rows2 = html.Tr(id='table_tr', children=[
                     html.Td(id='table_td', children=[
                         html.A(id='table_td_link',children=df.iloc[2][0],href=df.iloc[2][2])]),
-                    html.Td(id='table_td', children=[get_thumbs(df.iloc[2][1])]),
+                    html.Td(id='table_td', children=df.iloc[2][1]),
                 ])
 
     table_rows3 = html.Tr(id='table_tr', children=[
                     html.Td(id='table_td', children=[
                         html.A(id='table_td_link',children=df.iloc[3][0],href=df.iloc[3][2])]),
-                    html.Td(id='table_td', children=[get_thumbs(df.iloc[3][1])]),
+                    html.Td(id='table_td', children=df.iloc[3][1]),
                 ])
 
     table_rows4 = html.Tr(id='table_tr', children=[
                     html.Td(id='table_td', children=[
                         html.A(id='table_td_link',children=df.iloc[4][0],href=df.iloc[4][2])]),
-                    html.Td(id='table_td', children=[get_thumbs(df.iloc[4][1])]),
+                    html.Td(id='table_td', children=df.iloc[4][1]),
                 ])
 
     table_rows5 = html.Tr(id='table_tr', children=[
                     html.Td(id='table_td', children=[
                         html.A(id='table_td_link',children=df.iloc[5][0],href=df.iloc[5][2])]),
-                    html.Td(id='table_td', children=[get_thumbs(df.iloc[5][1])]),
+                    html.Td(id='table_td', children=df.iloc[5][1]),
                 ])
 
     return [html.Tbody([table_rows0, table_rows1, table_rows2, table_rows3, table_rows4, table_rows5])]
