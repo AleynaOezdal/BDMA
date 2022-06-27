610558e5cdf31c81435e32483821409e7780b652
Hannah Hielscher <hh057@hdm-stuttgart.de>
2022-06-27 14:48:50 +0200

    updated sentiment analysis

--- src/sentiment_analysis/sentiment_analysis_customerexperience.py
+++ src/sentiment_analysis/sentiment_analysis_customerexperience.py
@@ -1,105 +1,113 @@
-import pyspark
-import sparknlp
-from pyspark.sql import SparkSession
-from sparknlp.base import *
-from sparknlp.annotator import *
-from pyspark.ml import Pipeline
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType
 import pyspark.sql.functions as F
+from pyspark.sql.types import StringType
+from sparknlp.annotator import *
+from sparknlp.base import *
+from pyspark.ml import Pipeline
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# Setup Confluent Zugangsdaten
+confluentBootstrapServers = os.getenv('BOOTSTRAP.SERVERS')
+confluentApiKey = os.getenv('SASL.USERNAME')
+confluentSecret = os.getenv('SASL.PASSWORD')
 
 # Verbindung zu unserem MongoDB Cluster
-connectionString = f"mongodb+srv://{os.getenv('MONGODB.USERNAME')}:{os.getenv('MONGODB.PASSWORD')}@cluster0.hj2sr.mongodb.net/"
+connectionString = f"mongodb+srv://{os.getenv('MONGODB.USERNAME')}:{os.getenv('MONGODB.PASSWORD')}@bdma.rvryhyj.mongodb.net/"
 
-# Konfiguration von pyspark
-conf = pyspark.SparkConf()
-conf.set(
-    "spark.jars.packages",
-    "org.mongodb.spark:mongo-spark-connector:10.0.2,com.johnsnowlabs.nlp:spark-nlp_2.12:3.4.4",
+import sparknlp
+spark = sparknlp.start()
+
+# SparkNLP Model Pipeline:
+document = DocumentAssembler() \
+    .setInputCol("review") \
+    .setOutputCol("document")
+
+embeddings = BertSentenceEmbeddings\
+    .pretrained('labse', 'xx') \
+    .setInputCols(["document"]) \
+    .setOutputCol("sentence_embeddings")
+
+sentimentClassifier = ClassifierDLModel.pretrained("classifierdl_bert_sentiment", "de") \
+    .setInputCols(["document", "sentence_embeddings"]) \
+    .setOutputCol("class")
+
+nlpPipeline = Pipeline(stages=[
+ document,
+ embeddings,
+ sentimentClassifier
+ ])
+
+# Starten des Readstreams mit Datenquelle Kafka
+clickstreamTestDf = (
+  spark
+  .readStream
+  .format("kafka")
+  .option("kafka.bootstrap.servers", confluentBootstrapServers)
+  .option("kafka.security.protocol", "SASL_SSL")
+  .option("kafka.sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username='{}' password='{}';".format(confluentApiKey, confluentSecret))
+  .option("kafka.ssl.endpoint.identification.algorithm", "https")
+  .option("kafka.sasl.mechanism", "PLAIN")
+  .option("subscribe", "customer_experience")
+  .option("failOnDataLoss", "false")
+  .option("startingOffsets", "earliest")
+  .load()
 )
 
-# SparkSession mit allen benötigten Konfigurationen (MongoDB und SparkNLP) erstellen
-spark = (
-    SparkSession.builder.appName("Spark NLP")
-    .master("local[4]")
-    .config("spark.driver.memory", "16G")
-    .config("spark.driver.maxResultSize", "0")
-    .config("spark.kryoserializer.buffer.max", "2000M")
-    .config("spark.mongodb.read.connection.uri", connectionString)
-    .config("spark.mongodb.write.connection.uri", connectionString)
-    .config(conf=conf)
-    .getOrCreate()
-)
+newsStringDF = clickstreamTestDf.selectExpr("CAST(value AS STRING)")
 
-# Auslesen der Collection Customer_experience in Spark DataFrame
-df = (
-    spark.read.format("mongodb")
-    .option("database", "Company-Experience")
-    .option("collection", "Customer_experience")
-    .load()
-)
+# Daten in Schema laden
+schema = (
+        StructType()
+        .add("company", StringType())
+        .add("customer_exp", StructType()
+             .add("title", StringType())
+             .add("review", StringType())
+             .add("time", StringType())
+             .add("more info", StringType()))
+        .add("time", StringType())
+    )
 
-# DataFrame in gewünschte Struktur bringen, Verschachtelungen glätten
-df2 = df.select(
-    col("_id"),
+newsDF = newsStringDF.select(from_json(col("value"), schema).alias("data"))
+headlinesdf = newsDF.select("data.*")
+headlinesdf.printSchema()
+
+df2 = headlinesdf.select(col("company"),
     col("customer_exp.more info"),
     col("customer_exp.review"),
     col("customer_exp.title"),
     col("customer_exp.time"),
-    col("time"),
-)
-df2Flatten = df2.toDF("id", "more_info", "review", "title", "time", "timestamp")
+    col("time"))
+df2Flatten = df2.toDF("company","more_info","review","title","time","timestamp")
 
-# SparkNLP Model Pipeline:
-document = DocumentAssembler().setInputCol("review").setOutputCol("document")
+empty_df = spark.createDataFrame([['']]).toDF("text")
 
-embeddings = (
-    BertSentenceEmbeddings.pretrained("labse", "xx")
-    .setInputCols(["document"])
-    .setOutputCol("sentence_embeddings")
-)
-
-sentimentClassifier = (
-    ClassifierDLModel.pretrained("classifierdl_bert_sentiment", "de")
-    .setInputCols(["document", "sentence_embeddings"])
-    .setOutputCol("class")
-)
-
-nlpPipeline = Pipeline(stages=[document, embeddings, sentimentClassifier])
-
-# Leeren DataFrame für Ergebnisse erstellen
-empty_df = spark.createDataFrame([[""]]).toDF("text")
-
-# Pipeline auf leeren DataFrame fitten
+# Pipeline anwenden
 pipelineModel = nlpPipeline.fit(empty_df)
 
-# Pipeline auf DataFrame mit Daten anwenden
 result = pipelineModel.transform(df2Flatten)
 
-# DataFrame für Ergebnisse erstellen, welches an Datenbank gesendet werden kann: Enthält alle Infos wie EingangsDF plus Klasse
-df_results = result.select(
-    col("id"),
+# Finales Dataframe aufbauen
+df_results = result.select(col("company"),
     col("more_info"),
     col("review"),
     col("title"),
     col("time"),
     col("timestamp"),
-    col("class.result"),
-)
+    col("class.result"))
 
-df_resultsFlatten = df_results.toDF(
-    "id", "more_info", "review", "title", "time", "timestamp", "class"
-)
+df_resultsFlatten = df_results.toDF("company", "more_info", "review", "title", "time", "timestamp", "class")
 
-# Entfernen der eckigen Klammern um Klasse
-finished_df = df_resultsFlatten.withColumn("class", F.explode("class"))
-# finished_df.show()
+finished_df = df_resultsFlatten.withColumn('class', F.explode('class'))
 
-# DataFrame in Datenbank schreiben, collection Customer_experience_sentiment
-finished_df.write.format("mongodb").mode("append").option(
-    "database", "Company-Experience"
-).option("collection", "Customer_experience_sentiment").save()
+# Write Stream in MongoDB mit Checkpoint Location um Exactly-once Verarbeitung zu gewährleisten
+finished_df.writeStream \
+    .format("mongodb")\
+    .option("spark.mongodb.connection.uri", connectionString) \
+    .option("spark.mongodb.database", "Company-Environment") \
+    .option("spark.mongodb.collection", "customer_exp_gcp") \
+    .option("checkpointLocation", "gs://firstsparktest_1/checkpointsentimentcustexp") \
+    .outputMode("append") \
+    .start() \
+    .awaitTermination()
