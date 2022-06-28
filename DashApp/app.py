da5f269fcda6ed986a41abaa777c499343692fd8
kimrbg <kim-rautenberg@gmx.de>
2022-06-28 20:19:06 +0200

    updated port

--- DashApp/app.py
+++ DashApp/app.py
@@ -108,4 +108,5 @@ def render_page_content(pathname, value, date, time):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    # app.run_server(debug=True)
+    app.run_server(debug=True, host="0.0.0.0", port=8080)
